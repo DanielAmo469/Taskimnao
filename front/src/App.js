285ad8d3bfd26196a8da7b1f88643b49cf54884c
DanielAmo469 <danielamo469@gmail.com>
2025-02-10 12:01:57 +0200

    Finale

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Tasks from './components/Tasks';
+import Friends from './components/Friends';
+import MyAccound from './components/MyAccount'
+import CreateTask from './components/CreateTask';
+import TaskDetails from './components/Tasks/TaskDetails';
+import PrivateRoute from './components/PrivateRoute';
+import NotificationPage from './components/NotificationPage';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogin = (token) => {
+    setIsLoggedIn(true);
+    localStorage.setItem('token', token);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('token');
+    window.location.href = '/login';
+  };
+
   return (
-    <MDBContainer fluid>
-      <div
-        className='d-flex justify-content-center align-items-center'
-        style={{ height: '100vh' }}
-      >
-        <div className='text-center'>
-          <img
-            className='mb-4'
-            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className='mb-3'>
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className='mb-3'>MDB Team</p>
-          <MDBBtn
-            tag='a'
-            href='https://mdbootstrap.com/docs/standard/getting-started/'
-            target='_blank'
-            role='button'
-          >
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
-    </MDBContainer>
+    <Router>
+      <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+      <Routes>
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<PrivateRoute isLoggedIn={isLoggedIn}><Home /></PrivateRoute>} />
+        <Route path="/tasks" element={<PrivateRoute isLoggedIn={isLoggedIn}><Tasks /></PrivateRoute>} />
+        <Route path="/task/:id" element={<PrivateRoute isLoggedIn={isLoggedIn}><TaskDetails /></PrivateRoute>} />
+        <Route path="/friends" element={<PrivateRoute isLoggedIn={isLoggedIn}><Friends /></PrivateRoute>} />
+        <Route path='/my-account' element={<PrivateRoute isLoggedIn={isLoggedIn}><MyAccound /></PrivateRoute>} />
+        <Route path="/create-task" element={<PrivateRoute isLoggedIn={isLoggedIn}><CreateTask /></PrivateRoute>} />
+        <Route path="/notifications" element={<PrivateRoute isLoggedIn={isLoggedIn}><NotificationPage /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to={isLoggedIn ? "/home" : "/login"} />} />
+      </Routes>
+    </Router>
   );
 }
 
